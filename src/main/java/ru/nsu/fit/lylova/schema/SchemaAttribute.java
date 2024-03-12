b431b4d6475afd30c19f5d8e81ddb1a44833ea12
Sophya Lylova <s.lylova@g.nsu.ru>
2024-03-13 00:35:45 +0700

    change work with null arguments

--- src/main/java/ru/nsu/fit/lylova/schema/SchemaAttribute.java
+++ src/main/java/ru/nsu/fit/lylova/schema/SchemaAttribute.java
@@ -7,11 +7,20 @@ public class SchemaAttribute {
     private AttributeUse use = null;
 
     public SchemaAttribute(String name) {
+        if (name == null) {
+            throw new InvalidParameterException("name must be not null");
+        }
         this.name = name;
         this.use = AttributeUse.OPTIONAL;
     }
     public SchemaAttribute(String name, AttributeUse use) {
+        if (name == null) {
+            throw new InvalidParameterException("name must be not null");
+        }
         this.name = name;
+        if (use == null) {
+            throw new InvalidParameterException("use must be not null");
+        }
         this.use = use;
     }
     public void setName(String name) {
@@ -22,6 +31,9 @@ public class SchemaAttribute {
     }
 
     public void setUse(AttributeUse use) {
+        if (use == null) {
+            throw new InvalidParameterException("use must be not null");
+        }
         this.use = use;
     }
 
