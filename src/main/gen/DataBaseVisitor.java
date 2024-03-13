2b3891df8d950c40321ee3121f83cd82e4761f88
Sophya Lylova <s.lylova@g.nsu.ru>
2024-03-13 12:14:51 +0700

    fix

--- src/main/gen/DataBaseVisitor.java
+++ src/main/gen/DataBaseVisitor.java
@@ -1,4 +1,5 @@
 // Generated from C:/1/S_expr_data/src/main/antlr/Data.g4 by ANTLR 4.13.1
+
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 
 /**
@@ -7,57 +8,84 @@ import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
  * of the available methods.
  *
  * @param <T> The return type of the visit operation. Use {@link Void} for
- * operations with no return type.
+ *            operations with no return type.
  */
 @SuppressWarnings("CheckReturnValue")
 public class DataBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements DataVisitor<T> {
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitCalc(DataParser.CalcContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitExpr(DataParser.ExprContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitExpr_name(DataParser.Expr_nameContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitAttrs(DataParser.AttrsContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitAttr(DataParser.AttrContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitAttr_name(DataParser.Attr_nameContext ctx) { return visitChildren(ctx); }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation returns the result of calling
-	 * {@link #visitChildren} on {@code ctx}.</p>
-	 */
-	@Override public T visitValue(DataParser.ValueContext ctx) { return visitChildren(ctx); }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitCalc(DataParser.CalcContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitExpr(DataParser.ExprContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitExpr_name(DataParser.Expr_nameContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitAttrs(DataParser.AttrsContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitAttr(DataParser.AttrContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitAttr_name(DataParser.Attr_nameContext ctx) {
+        return visitChildren(ctx);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation returns the result of calling
+     * {@link #visitChildren} on {@code ctx}.</p>
+     */
+    @Override
+    public T visitValue(DataParser.ValueContext ctx) {
+        return visitChildren(ctx);
+    }
 }
--- src/main/gen/DataVisitor.java
+++ src/main/gen/DataVisitor.java
@@ -1,4 +1,5 @@
 // Generated from C:/1/S_expr_data/src/main/antlr/Data.g4 by ANTLR 4.13.1
+
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
 /**
@@ -6,49 +7,62 @@ import org.antlr.v4.runtime.tree.ParseTreeVisitor;
  * by {@link DataParser}.
  *
  * @param <T> The return type of the visit operation. Use {@link Void} for
- * operations with no return type.
+ *            operations with no return type.
  */
 public interface DataVisitor<T> extends ParseTreeVisitor<T> {
-	/**
-	 * Visit a parse tree produced by {@link DataParser#calc}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitCalc(DataParser.CalcContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link DataParser#expr}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitExpr(DataParser.ExprContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link DataParser#expr_name}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitExpr_name(DataParser.Expr_nameContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link DataParser#attrs}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitAttrs(DataParser.AttrsContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link DataParser#attr}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitAttr(DataParser.AttrContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link DataParser#attr_name}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitAttr_name(DataParser.Attr_nameContext ctx);
-	/**
-	 * Visit a parse tree produced by {@link DataParser#value}.
-	 * @param ctx the parse tree
-	 * @return the visitor result
-	 */
-	T visitValue(DataParser.ValueContext ctx);
+    /**
+     * Visit a parse tree produced by {@link DataParser#calc}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitCalc(DataParser.CalcContext ctx);
+
+    /**
+     * Visit a parse tree produced by {@link DataParser#expr}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitExpr(DataParser.ExprContext ctx);
+
+    /**
+     * Visit a parse tree produced by {@link DataParser#expr_name}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitExpr_name(DataParser.Expr_nameContext ctx);
+
+    /**
+     * Visit a parse tree produced by {@link DataParser#attrs}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitAttrs(DataParser.AttrsContext ctx);
+
+    /**
+     * Visit a parse tree produced by {@link DataParser#attr}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitAttr(DataParser.AttrContext ctx);
+
+    /**
+     * Visit a parse tree produced by {@link DataParser#attr_name}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitAttr_name(DataParser.Attr_nameContext ctx);
+
+    /**
+     * Visit a parse tree produced by {@link DataParser#value}.
+     *
+     * @param ctx the parse tree
+     * @return the visitor result
+     */
+    T visitValue(DataParser.ValueContext ctx);
 }
