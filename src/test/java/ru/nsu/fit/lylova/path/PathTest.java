3f9c0d1dbca38458c82d562f3c5885e2fb11e462
Sophya Lylova <s.lylova@g.nsu.ru>
2024-03-27 01:56:35 +0700

    add tests for Path.compile()

--- src/test/java/ru/nsu/fit/lylova/path/PathTest.java
+++ src/test/java/ru/nsu/fit/lylova/path/PathTest.java
@@ -5,7 +5,6 @@ import ru.nsu.fit.lylova.data.DataReader;
 import ru.nsu.fit.lylova.data.node.ElementNode;
 import ru.nsu.fit.lylova.data.node.Node;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -38,5 +37,89 @@ class PathTest {
             correctResult.remove(id);
         }
         assertTrue(correctResult.isEmpty());
+    }
+
+    @Test
+    void compile() throws Exception {
+        Path path = Path.compile("kek");
+        assertEquals(PathType.RELATIVE, path.getType());
+
+        path = Path.compile("/kek");
+        assertEquals(PathType.ABSOLUTE, path.getType());
+
+        path = Path.compile("//kek");
+        assertEquals(PathType.ABSOLUTE, path.getType());
+        assertEquals(2, path.getSteps().size());
+        assertEquals(StepTransition.JUST_CURRENT, path.getSteps().get(0).getTransition());
+        assertEquals(AxisType.CURRENT, path.getSteps().get(0).getAxisType());
+        assertEquals(StepTransition.ANY_INNER, path.getSteps().get(1).getTransition());
+
+        path = Path.compile("el/kek//do");
+        assertNotNull(path);
+        assertEquals(3, path.getSteps().size());
+
+        path = Path.compile(".");
+        assertEquals(1, path.getSteps().size());
+        Step step = path.getSteps().get(0);
+        assertEquals(AxisType.CURRENT, step.getAxisType());
+        assertNull(step.getRequirement());
+        assertEquals(StepTransition.JUST_CURRENT, step.getTransition());
+
+        path = Path.compile("kek/kek2");
+        assertEquals(2, path.getSteps().size());
+        assertEquals(StepTransition.JUST_CURRENT, path.getSteps().get(1).getTransition());
+
+        path = Path.compile("kek//kek2");
+        assertEquals(2, path.getSteps().size());
+        assertEquals(StepTransition.ANY_INNER, path.getSteps().get(1).getTransition());
+
+        path = Path.compile("*");
+        assertEquals(1, path.getSteps().size());
+        assertEquals(AxisType.CHILD, path.getSteps().get(0).getAxisType());
+
+        path = Path.compile("@value");
+        assertEquals(1, path.getSteps().size());
+        assertEquals(AxisType.CHILD_VALUE, path.getSteps().get(0).getAxisType());
+
+        path = Path.compile("@element");
+        assertEquals(1, path.getSteps().size());
+        assertEquals(AxisType.CHILD_ELEMENT, path.getSteps().get(0).getAxisType());
+
+        path = Path.compile(".");
+        assertEquals(1, path.getSteps().size());
+        assertEquals(AxisType.CURRENT, path.getSteps().get(0).getAxisType());
+
+        path = Path.compile("..");
+        assertEquals(1, path.getSteps().size());
+        assertEquals(AxisType.PARENT, path.getSteps().get(0).getAxisType());
+
+        path = Path.compile("*[pred(\"kek\", @name)]");
+        assertEquals(1, path.getSteps().size());
+        step = path.getSteps().get(0);
+        assertNotNull(step.getRequirement());
+        assertEquals("name", step.getRequirement().getRightPart());
+        assertEquals(RequirementPartType.ATTR_NAME, step.getRequirement().getRightPartType());
+        assertEquals(RequirementPartType.VALUE, step.getRequirement().getLeftPartType());
+        assertEquals("pred", step.getRequirement().getPredicateName());
+
+        path = Path.compile("*[@a1 != @a2]");
+        assertEquals(1, path.getSteps().size());
+        step = path.getSteps().get(0);
+        assertNotNull(step.getRequirement());
+        assertEquals("a2", step.getRequirement().getRightPart());
+        assertEquals(RequirementPartType.ATTR_NAME, step.getRequirement().getRightPartType());
+        assertEquals("a1", step.getRequirement().getLeftPart());
+        assertEquals(RequirementPartType.ATTR_NAME, step.getRequirement().getLeftPartType());
+        assertEquals(PredicateType.INEQUALITY, step.getRequirement().getPredicateType());
+
+        path = Path.compile("*[@* = @*]");
+        assertEquals(1, path.getSteps().size());
+        step = path.getSteps().get(0);
+        assertNotNull(step.getRequirement());
+        assertEquals("*", step.getRequirement().getRightPart());
+        assertEquals(RequirementPartType.ATTR_NAME, step.getRequirement().getRightPartType());
+        assertEquals("*", step.getRequirement().getLeftPart());
+        assertEquals(RequirementPartType.ATTR_NAME, step.getRequirement().getLeftPartType());
+        assertEquals(PredicateType.EQUALITY, step.getRequirement().getPredicateType());
     }
 }
